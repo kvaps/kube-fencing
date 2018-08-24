d11317ce88c57c08d5708711f6ead50e9668b9d0
kvaps <kvapss@gmail.com>
2018-08-24 16:49:52 +0200

    add: params getting

--- fencing-controller/fencing-controller.sh
+++ fencing-controller/fencing-controller.sh
@@ -14,10 +14,13 @@ die () {
 
 echo "Starting loop"
 unset column
-kubectl get node -o json -w -l fencing=enabled | 
+# kubectl get node -o 'custom-columns=NAME:.metadata.name,STATUS:.status.conditions[?(@.type=="Ready")].status'
+kubectl get node -o json -w  | 
   jq --raw-output --unbuffered '
   .metadata.name,
-  (.status.conditions[] | select(.type=="Ready") | .status)' |
+  .metadata.creationTimestamp,
+  (.status.conditions[] | select(.type=="Ready") | .status),
+  (.status.conditions[] | select(.type=="Ready") | .lastHeartbeatTime)' |
 while read string; do
   column=$((column+1))
   case "${column}" in
@@ -26,18 +29,22 @@ while read string; do
       continue
       ;;
     2)
-      ready="${string}"
+      status="${string}"
       unset column
       ;;
   esac
-;;
 
-  if [ "${ready}" != "False" ]
-  then
+  if [ "${status}" = "True" ]; then
       continue
   fi
 
   #FENCING_AGENT=$(kubectl get pod -l "${FENCING_AGENT_SELECTOR}" | awk '$3 == "Running" {print $1}' | head -n1)
+
+  #kubectl get node -o json "$node" | jq 'del(.status)' > /tmp/node.json
+  #fence_command
+  #kubectl delete node "$node"
+  #kubectl create -f /tmp/node.json
+
 
 done
 
