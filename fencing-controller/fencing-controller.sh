2a37662bc1208a0aa89ffd0146229bb1ba2ba924
kvaps <kvapss@gmail.com>
2018-08-25 17:21:50 +0200

    add: debug information and main functions

--- fencing-controller/fencing-controller.sh
+++ fencing-controller/fencing-controller.sh
@@ -1,9 +1,5 @@
 #!/bin/sh
 
-debug() {
-  [ -n "$DEBUG" ] && echo $(date '+%b %d %X') "debug: $@"
-}
-
 log() {
   echo $(date '+%b %d %X') "info:  $@"
 }
@@ -15,15 +11,67 @@ warn() {
 die() {
   status="$1"
   shift
-  echo $(date '+%b %d %X') "fatal: $@" >&2
+  echo $(date '+%b %d %X') "error: $@" >&2
   exit "$status"
 }
 
+debug() {
+  [ -n "$DEBUG" ] && echo $(date '+%b %d %X') "debug: $@" >&2
+}
+
+# Run external command
+run() {
+  set -e
+  ( [ -n "$DEBUG" ] && set -x; "$@" )
+  set +e
+}
+
+# Fencing node via fencing agent pod
+fence() {
+  log "Fencing node $1"
+  FENCING_AGENT_POD=$(run kubectl get pod -l "${FENCING_AGENT_SELECTOR}" | awk '$3 == "Running" {print $1}' | head -n1)
+  if [ -z "$FENCING_AGENT_POD" ]; then
+    die 1 "Can not find running fencing agent pod"
+  fi
+  debug "FENCING_AGENT_POD: $FENCING_AGENT_POD"
+  run kubectl exec "$FENCING_AGENT_POD" -- $FENCING_SCRIPT "$1"
+  if [ $? -eq 126 ]; then
+    die 2 "Can not find fencing script"
+  fi
+}
+
+# Flush node in kubernetes
+flush() {
+  set -e
+  case "$FLUSHING_MODE" in
+    delete)
+      log "Deleting node $1"
+      run kubectl delete node "$1"
+    ;;
+    recreate)
+      log "Recreating node $1"
+      rm -f /tmp/node.json
+      run kubectl get node -o json "$1" | jq 'del(.status)' > /tmp/node.json
+      run kubectl delete node "$1"
+      run kubectl create -f /tmp/node.json
+    ;;
+  esac
+  set +e
+}
+
 main() {
-  debug "FENCING_AGENT_SELECTOR=$FENCING_AGENT_SELECTOR"
-  debug "FENCING_NODE_SELECTOR=$FENCING_NODE_SELECTOR"
+  log "Loading parameters"
+  FENCING_NODE_SELECTOR=${FENCING_NODE_SELECTOR:-fencing=enabled}
+  FENCING_AGENT_SELECTOR=${FENCING_AGENT_SELECTOR:-app=fencing-agent}
+  FENCING_SCRIPT=${FENCING_SCRIPT:-/scripts/fence}
+  FLUSHING_MODE=${FLUSHING_MODE:-delete}
+  log "FENCING_NODE_SELECTOR: $FENCING_NODE_SELECTOR"
+  log "FENCING_AGENT_SELECTOR: $FENCING_AGENT_SELECTOR"
+  log "FENCING_SCRIPT: $FENCING_SCRIPT"
+  log "FLUSHING_MODE: $FLUSHING_MODE"
+
   log "Starting loop"
-  kubectl get node -w -l "$FENCING_NODE_SELECTOR" | 
+  run kubectl get node -w -l "$FENCING_NODE_SELECTOR" | 
   while read line; do
     while read NAME STATUS ROLES AGE VERSION; do
       debug "$NAME - $STATUS"
@@ -33,14 +81,13 @@ main() {
       REASON=$(kubectl get node "$NAME" -o 'custom-columns=STATUS:.status.conditions[?(@.type=="Ready")].reason' | tail -n1)
       if [ "$REASON" = "NodeStatusUnknown" ]; then
         log "$NAME - $REASON"
+        fence "$NAME"
+        if [ $? -eq 0 ]; then
+          flush "$NAME"
+        fi
       fi
     done
   done
 }
 
-  #FENCING_AGENT=$(kubectl get pod -l "${FENCING_AGENT_SELECTOR}" | awk '$3 == "Running" {print $1}' | head -n1)
-
-  #kubectl get node -o json "$node" | jq 'del(.status)' > /tmp/node.json
-  #fence_command
-  #kubectl delete node "$node"
-  #kubectl create -f /tmp/node.json
+main "$@"
