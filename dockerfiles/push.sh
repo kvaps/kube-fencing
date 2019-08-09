6135d7f9df16c4bde2f9e632caa53c013343c536
kvaps <kvapss@gmail.com>
2019-08-09 16:51:45 +0200

    do not use buildkit

--- dockerfiles/push.sh
+++ dockerfiles/push.sh
@@ -3,6 +3,5 @@
 cd $(dirname $0)
 
 for image in $(find . -maxdepth 2 -name Dockerfile | awk -F/ '{print $2}'); do
-  export DOCKER_BUILDKIT=1
   docker push ${CI_REGISTRY_IMAGE:-kvaps}/kube-$image:${CI_COMMIT_REF_NAME:-latest}
 done
