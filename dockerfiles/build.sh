13e27ca98120e5fb5b9f0c3e739b0af8a4717a88
kvaps <kvapss@gmail.com>
2019-08-16 02:19:06 +0200

    upd: build.sh push.sh

--- dockerfiles/build.sh
+++ dockerfiles/build.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
+set -e
 
 cd $(dirname $0)
 
 for image in $(find . -maxdepth 2 -name Dockerfile | awk -F/ '{print $2}'); do
-  docker build --build-arg VERSION=${CI_COMMIT_REF_NAME-latest} -t ${CI_REGISTRY_IMAGE:-kvaps}/kube-$image:${CI_COMMIT_REF_NAME:-latest} $image
+  (set -x; docker build --build-arg VERSION=${CI_COMMIT_REF_NAME-latest} -t ${CI_REGISTRY_IMAGE:-kvaps}/kube-$image:${CI_COMMIT_REF_NAME:-latest} $image)
 done
--- dockerfiles/push.sh
+++ dockerfiles/push.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
+set -e
 
 cd $(dirname $0)
 
 for image in $(find . -maxdepth 2 -name Dockerfile | awk -F/ '{print $2}'); do
-  docker push ${CI_REGISTRY_IMAGE:-kvaps}/kube-$image:${CI_COMMIT_REF_NAME:-latest}
+  (set -x; docker push ${CI_REGISTRY_IMAGE:-kvaps}/kube-$image:${CI_COMMIT_REF_NAME:-latest})
 done
